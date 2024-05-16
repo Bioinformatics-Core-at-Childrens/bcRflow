f4106669039341fd125304fc583b2cc258f70157
= <=>
2024-05-16 14:58:01 -0400

    update example submission script

--- workflow/bcRflow.sh
+++ workflow/bcRflow.sh
@@ -9,6 +9,7 @@
 #SBATCH --mail-type=END
 #SBATCH --mail-user=your_email@sample.com
 
-module load singularity/3.9.6 nextflow/23.04.2 squashfs-tools/4.4 gcc/12.2.0 r/4.3.0
+module load singularity/3.9.6 nextflow/23.04.2 squashfs-tools/4.4 gcc/12.2.0
+
 cd bcRflow/workflow
 nextflow run ./main.nf -profile slurm -resume -work-dir ./work
